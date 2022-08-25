c5cc52b3a74c3e4b52de8d944aef3569bf433e07
kamilspyrka <kamilspyrka12@gmail.com>
2022-08-25 10:46:41 +0200

    Backend&FrontEnd Validation

---
+++ server/policies/CarControllerPolicy.js
@@ -0,0 +1,49 @@
+const Joi = require('joi')
+
+const dt = new Date();
+dt.setDate(dt.getDate()+1)
+today = dt.getFullYear() + "-" + (dt.getMonth() + 1) + "-" + dt.getDate();
+
+module.exports = {
+  validate (req, res, next) {
+    const schema = Joi.object({
+        producer: Joi.string() .required(),
+        model: Joi.string() .required(),
+        prodDate: Joi.date() .max(today) .min('1900-01-01').required(),
+        purchaseDate: Joi.date() .max(today) .min('1970-01-01').required(),
+        mileage: Joi.number() .min(0) .required()
+    })
+
+    const {error} = schema.validate(req.body)
+    
+    if (error) {
+      switch (error.details[0].context.key) {
+        case 'prodDate':
+          res.status(400).send({
+            error: `Production date cant be in the future, nor before 1900`
+          })
+        break
+
+        case 'purchaseDate':
+          res.status(400).send({
+              error: `Purchase date cant be in the future, nor before 1970`
+          })
+        break
+
+        case 'mileage':
+          res.status(400).send({
+              error: 'Mileage cannot be negative number'
+          })
+        break
+
+        default:
+          res.status(400).send({
+            error: 'Error has occured'
+          })
+      }
+    }
+    else {
+      next()
+    }
+  }
+}
--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,7 @@
 const AuthController = require('./controllers/AuthController')
 const CarController = require('./controllers/CarController')
 const AuthControllerPolicy = require('./policies/AuthControllerPolicy')
+const CarControllerPolicy = require('./policies/CarControllerPolicy')
 const EnsureAuth = require('./controllers/EnsureAuth')
 
 module.exports = (app) => {
@@ -11,10 +12,17 @@ module.exports = (app) => {
     app.post('/login', AuthController.login)
 
     //Cars
-    app.get('/cars', EnsureAuth,  CarController.allCars)
-    app.post('/add', EnsureAuth, CarController.addCar)
+    app.get('/cars',EnsureAuth, CarController.allCars)
     app.get('/cars/:carId', EnsureAuth,  CarController.showCar)
-    app.put('/cars/:carId', EnsureAuth,  CarController.editCar)
     app.delete('/cars/:carId', EnsureAuth, CarController.deleteCar)
+    
+    app.post('/add', EnsureAuth,
+    CarControllerPolicy.validate,
+    CarController.addCar)
+    app.put('/cars/:carId', EnsureAuth,
+    CarControllerPolicy.validate,
+    CarController.editCar)
+
+
 
 }
