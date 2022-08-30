ebae9bdeec7ac663f54eacaff14fe644a1514d79
kamilspyrka <kamilspyrka12@gmail.com>
2022-08-30 10:25:47 +0200

    prettier

--- server/config/config.js
+++ server/config/config.js
@@ -1,17 +1,16 @@
 module.exports = {
-    port: process.env.PORT || 5000,
-    db: {
-        database: process.env.DB_NAME || 'CarFleet',
-        user: process.env.DB_USER || 'CarFleet',
-        password: process.env.DB_PASS || 'CarFleet',
-        options: {
-            dialect: process.env.DIALECT || 'sqlite',
-            host: process.env.HOST || 'localhost',
-            storage: './CarFleet.sqlite'
-        }
+  port: process.env.PORT || 5000,
+  db: {
+    database: process.env.DB_NAME || "CarFleet",
+    user: process.env.DB_USER || "CarFleet",
+    password: process.env.DB_PASS || "CarFleet",
+    options: {
+      dialect: process.env.DIALECT || "sqlite",
+      host: process.env.HOST || "localhost",
+      storage: "./CarFleet.sqlite",
     },
-    authentication: {
-        jwtSecret: process.env.JWT_SECRET || 'secret'
-      }
-
-}
+  },
+  authentication: {
+    jwtSecret: process.env.JWT_SECRET || "secret",
+  },
+};
