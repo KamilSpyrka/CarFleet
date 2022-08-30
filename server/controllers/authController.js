06c0c0ae20a4ebe66c255e5f46df25e318c52441
kamilspyrka <kamilspyrka12@gmail.com>
2022-08-30 21:42:52 +0200

    heroku

---
+++ server/controllers/authController.js
@@ -0,0 +1,75 @@
+const { User } = require("../models");
+const jwt = require("jsonwebtoken");
+const config = require("../config/config");
+
+function jwtSignUser(user) {
+  const ONE_WEEK = 60 * 60 * 24 * 7;
+  return jwt.sign(user, config.authentication.jwtSecret, {
+    expiresIn: ONE_WEEK,
+  });
+}
+
+let options = {
+  path: "/",
+  sameSite: true,
+  maxAge: 1000 * 60 * 60 * 24, // would expire after 24 hours
+  httpOnly: true, // The cookie only accessible by the web server
+};
+
+module.exports = {
+  async register(req, res) {
+    try {
+      const user = await User.create(req.body);
+      const userJson = user.toJSON();
+      delete userJson.password;
+
+      res.cookie("x-access-token", jwtSignUser(userJson), options);
+      res.send({
+        user: userJson,
+        token: jwtSignUser(userJson),
+      });
+    } catch (err) {
+      res.status(400).send({
+        error: "This e-mail is already registered",
+      });
+    }
+  },
+
+  async login(req, res) {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({
+        where: {
+          email: email,
+        },
+      });
+
+      if (!user) {
+        return res.status(401).send({
+          error: "The login information was incorrect",
+        });
+      }
+
+      const isPasswordValid = await user.comparePassword(password);
+
+      if (!isPasswordValid) {
+        return res.status(401).send({
+          error: "The login information was incorrect",
+        });
+      }
+
+      const userJson = user.toJSON();
+      delete userJson.password;
+
+      res.cookie("x-access-token", jwtSignUser(userJson), options);
+      res.send({
+        user: userJson,
+        token: jwtSignUser(userJson),
+      });
+    } catch (err) {
+      res.status(500).send({
+        error: err.message,
+      });
+    }
+  },
+};
---
+++ server/policies/authControllerPolicy.js
@@ -0,0 +1,55 @@
+const Joi = require("joi");
+const { joiPasswordExtendCore } = require("joi-password");
+const joiPassword = Joi.extend(joiPasswordExtendCore);
+module.exports = {
+  validate(req, res, next) {
+    const schema = Joi.object({
+      firstName: Joi.string().required(),
+      lastName: Joi.string().required(),
+      email: Joi.string().max(255).required().email(),
+      password: joiPassword
+        .string()
+        .min(6)
+        .required()
+        .minOfNumeric(1)
+        .minOfSpecialCharacters(1),
+    });
+
+    const { error } = schema.validate(req.body);
+
+    if (error) {
+      switch (error.details[0].context.key) {
+        case "email":
+          res.status(400).send({
+            error: `You must provide a valid email address`,
+          });
+          break;
+
+        case "password":
+          res.status(400).send({
+            error: `Password should be at least 6 characters long, contain 1 number and 1 special sign`,
+          });
+          break;
+
+        case "firstName":
+          res.status(400).send({
+            error: `You must provide a valid first name`,
+          });
+          break;
+
+        case "lastName":
+          res.status(400).send({
+            error: `You must provide a valid last name`,
+          });
+          break;
+
+        default:
+          res.status(400).send({
+            error: "Invalid registration information",
+          });
+      }
+    } else {
+      next();
+    }
+  },
+};
